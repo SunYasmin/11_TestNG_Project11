d7d2c72bdccbdf56d4f93253b109f359196691d0
Derya Akpinar <derya.akpinar2017@gmail.com>
2022-10-26 00:21:33 +0300

    ikinciguncelleme

---
+++ src/test/java/tests/US_18/TC_002.java
@@ -0,0 +1,43 @@
+package tests.US_18;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DeryaPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class TC_002 {
+    DeryaPage storeManager=new DeryaPage();
+
+    @Test(priority = 1)
+    public void test2_refundRequest() {
+
+        ReusableMethods.myAccount();
+        storeManager.storeMan.click();
+        ReusableMethods.waitFor(2);
+        ReusableMethods.scrollIntoView(storeManager.refundButtonu);
+        ReusableMethods.waitFor(2);
+        storeManager.refundButtonu.click();
+        ReusableMethods.scrollIntoView(storeManager.refundRequest);
+        storeManager.refundRequest.isDisplayed();
+        //Refund Request sayfasindaki Tablonun başlıklarıni konsolda yazdır.
+        System.out.println("Iade Tablosundaki Basliklar" + storeManager.iadeTablosu.getText());  //Basliklar :Request ID Order ID Store Amount Typ
+
+        //Order ID  basliginin altindaki tum degerleri yazdir.
+        String orderIDValue="";
+        for (WebElement w: storeManager.orderIDListesi) {
+            orderIDValue+=w.getText() + ", ";
+            System.out.println("Order ID Value: " + w.getText());
+        }
+        Assert.assertTrue(orderIDValue.contains("#14094")); //Passed
+
+
+    }
+
+    @Test(priority = 2)
+    public void testKapatma() {
+        Driver.quitDriver();
+    }
+
+}
---
+++ src/test/java/tests/US_18/TC_004.java
@@ -0,0 +1,48 @@
+package tests.US_18;
+
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.DeryaPage;
+import utilities.ReusableMethods;
+
+public class TC_004 {
+    DeryaPage storeManager=new DeryaPage();
+
+    @Test
+    public void test04_refundRequest() {
+
+        ReusableMethods.myAccount();
+        storeManager.storeMan.click();
+        ReusableMethods.waitFor(2);
+        ReusableMethods.scrollIntoView(storeManager.refundButtonu);
+        ReusableMethods.waitFor(2);
+        storeManager.refundButtonu.click();
+        ReusableMethods.scrollIntoView(storeManager.refundRequest);
+        storeManager.refundRequest.isDisplayed();
+        //Refund Request sayfasindaki Tablonun başlıklarıni konsolda yazdır.
+        System.out.println("Iade Tablosundaki Basliklar" + storeManager.iadeTablosu.getText());  //Basliklar :Request ID Order ID Store Amount Typ
+
+        System.out.println("=================================================================================");
+
+        //Type  basliginin altindaki tum degerleri yazdir.
+        String typeValue="";
+        for (WebElement w:storeManager.typeListesi) {
+            System.out.println("Type Value: " + w.getText());
+            typeValue+=w.getText() + ", ";
+        }
+        Assert.assertTrue(typeValue.contains("Partial Refund"));
+
+        System.out.println("=====================================================================================");
+
+        //Reason  basliginin altindaki tum degerleri yazdir.
+        String reasonValue="";
+        for (WebElement w:storeManager.reasonListesi) {
+            System.out.println("Reason Value: " +w.getText());
+            reasonValue+=w.getText() + ", ";
+
+        }
+        Assert.assertTrue(reasonValue.contains("Ürünün ambalajı yırtıktı"));
+
+    }
+}
